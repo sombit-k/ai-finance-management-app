ca6a8b796d34275730a039f8bb25e3ab17550e5f
sombit-k <sombitkarmakar018@gmail.com>
2025-07-05 14:26:06 +0530

    fix: Update import statement for Toaster component to use direct package reference

--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,7 @@ import { Noto_Sans } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/header";
 import { ClerkProvider } from "@clerk/nextjs";
-import { Toaster } from "@/components/ui/sonner";
+import { Toaster } from "sonner";
 
 const notoSans = Noto_Sans({
   subsets: ["latin"],
