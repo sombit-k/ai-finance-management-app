b059fc02c19e8c08055782fed71ae35e9f6168f6
sombit-k <sombitkarmakar018@gmail.com>
2025-07-05 14:02:57 +0530

    fix: Adjust indentation and formatting in next.config.mjs for consistency

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
+  images: {
     remotePatterns: [
       {
         protocol: "https",
         hostname: "randomuser.me",
       },
     ],
+  },
+
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "5mb",
+    },
   },
 };
 
