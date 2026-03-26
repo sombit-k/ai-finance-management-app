4e8c080d5bace55727f15e334aadfd3c916529fd
sombit-k <sombitkarmakar018@gmail.com>
2026-03-26 11:49:22 +0530

    fix: Correct spacing in AuthLayout component and update email script in package.json

--- app/(auth)/layout.js
+++ app/(auth)/layout.js
@@ -1,6 +1,6 @@
 
 export default function AuthLayout({children}) {
     return (
-        <div className="flex justify-center py-40 ">{children}</div>
+        <div className="flex justify-center py- 40 ">{children}</div>
     )
 }
